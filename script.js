0651ab04eaf9075e046d85c041b49fea6e50d55b
Lauren Go <go.lauren@berkeley.edu>
2019-12-04 18:27:22 -0800

    working...?

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function nameClicked(name) {
     hideNames();
-    // var x = document.getElementById(name);
-    // x.style.display="block"
+    var x = document.getElementById(name);
+    x.style.display="block"
 }
 function hideNames() {
     var names = ["pierre", "lauren", "emmanuel", "george", "peter"];
     for(var i=0; i < names.length; i++) {
         var x = document.getElementById(names[i]);
-        x.style.display ="none";
+        if (x != null) {
+            x.style.display ="none";
+        }
     }
 }

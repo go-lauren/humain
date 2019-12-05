f62d2a7be1e950411b1578087fb4e91fc08348a9
Lauren Go <go.lauren@berkeley.edu>
2019-12-05 08:14:10 -0800

    help

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+window.onload = 
+function(){
+    names = ["pierre", "lauren", "emmanuel", "george", "peter"];
+    fullNames = ["Pierre Amougou", "Lauren Go", "Emmanuel Kinzonzi", "George Wang", "Peter Wang"]
+    setupNameBar();
+};
+
+function setupNameBar() {
+    var divcontent = "";
+    for (var i=0; i < names.length; i++) {
+        divcontent += `\<div class=\"name\"\>`;
+        divcontent += `${fullNames[i]}`;
+        divcontent += `\<\/div\>`
+    }
+    var nameBar = document.getElementsByClassName("name-bar");
+    nameBar[0].innerHTML = divcontent;
+}
 function nameClicked(name) {
     hideNames();
     var x = document.getElementById(name);
     x.style.display="block"
 }
+
 function hideNames() {
-    var names = ["pierre", "lauren", "emmanuel", "george", "peter"];
     for(var i=0; i < names.length; i++) {
         var x = document.getElementById(names[i]);
         if (x != null) {
